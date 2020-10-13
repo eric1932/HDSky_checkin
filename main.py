86a2eee3477b470f8aa0b1b253905cf306c5d0db
Eric <eric1932777@gmail.com>
2020-10-13 12:35:57 -0500

    部分增加重试功能

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from baidu_api import APP_ID, API_KEY, SECRET_KEY
 
+MAX_RETRY = 5
+
 
 def get_driver(browser: str, headless=False, proxy=None):
     if "chrome".startswith(browser.lower()):
@@ -82,30 +84,45 @@ if __name__ == '__main__':
         logged_in = True
 
     if not logged_in:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "showupimg")))
-        captcha_url = driver.find_element_by_id("showupimg").get_attribute("src")
+        retry_count = 0
+        while True:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "showupimg")))
+            captcha_url = driver.find_element_by_id("showupimg").get_attribute("src")
 
-        r = requests.get(captcha_url)
-        image = np.asarray(bytearray(r.content))
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        png_cv_bytes = convert(image)
+            r = requests.get(captcha_url)
+            image = np.asarray(bytearray(r.content))
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+            png_cv_bytes = convert(image)
 
-        # result = ocr_client.basicAccurate(png_cv_bytes)['words_result'][0]['words']
-        result = ocr_client.basicGeneral(png_cv_bytes)['words_result'][0]['words']
-        result = re.sub('[\W_]+', '', result)  # 只保留字母和数字
-        print(result)  # debug
-        if len(result) != 6:  # 验证码只有6位长
-            pass
-
-        driver.find_element_by_id("imagestring").send_keys(result)
-        time.sleep(0.2)
-        driver.find_element_by_id("showupbutton").click()
-        time.sleep(0.5)  # TODO 增加延时以等待结果
+            # result = ocr_client.basicAccurate(png_cv_bytes)['words_result'][0]['words']
+            result = ocr_client.basicGeneral(png_cv_bytes)['words_result'][0]['words']
+            result = re.sub(r'[\W_]+', '', result)  # 只保留字母和数字
+            result = result.upper()  # 只有大写字母
+            print(result)  # debug
+            if len(result) == 6:  # 验证码只有6位长
+                driver.find_element_by_id("imagestring").send_keys(result)
+                time.sleep(0.2)
+                driver.find_element_by_id("showupbutton").click()
+                time.sleep(0.5)  # TODO 增加延时以等待结果
+                break  # 退出while
+            elif retry_count == MAX_RETRY:
+                print("重试到达上限")
+                # TODO
+                driver.close()
+                exit(1)
+            else:
+                print("验证码格式有误")
+                # redo
+                driver.find_element_by_class_name("layui-layer-setwin").click()
+                time.sleep(0.5)
+                driver.find_element_by_id("showup").click()
+                retry_count += 1
 
         # check result
         result = driver.find_element_by_class_name("layui-layer-content").text
         if result == "验证码错误(Wrong CAPTCHA)":
             print(result[:5])
+            # TODO 重试
         else:
             match = re.search(r"成功,魔力值加([0-9]+)", result)
             print(match.group(0))
